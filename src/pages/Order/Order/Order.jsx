f8034cef4021793f1dd52a9afb1e82c776f644eb
biswasbittu <biswasbittu7415@yahoo.com>
2023-06-02 13:16:04 +0530

    card done

--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -2,27 +2,39 @@ import { useState } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import orderCoverImg from "../../../assets/shop/order.jpg";
+import useMenu from "../../../hooks/useMenu";
 import Cover from "../../Shared/Cover/Cover";
 const Order = () => {
-    const [tabIndex,setTabIndex]=useState(0)
+  const [tabIndex, setTabIndex] = useState(0);
+  const [menu] = useMenu();
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const soup = menu.filter((item) => item.category === "soup");
+  const salad = menu.filter((item) => item.category === "salad");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
+
   return (
     <div className="">
       <Cover img={orderCoverImg} title={"ORDER Food"} />
       <div className="text-center font-[Inter]">
-      <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
-        <TabList >
-          <Tab >Salad</Tab>
-          <Tab>Pizza</Tab>
-          <Tab>soups</Tab>
-          <Tab>desserts</Tab>
-          <Tab>drinks</Tab>
-        </TabList>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
-      </Tabs>
+        <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+          <TabList>
+            <Tab>Salad</Tab>
+            <Tab>Pizza</Tab>
+            <Tab>soups</Tab>
+            <Tab>desserts</Tab>
+            <Tab>drinks</Tab>
+          </TabList>
+          <TabPanel>
+            {
+             salad.map() 
+            }
+          </TabPanel>
+          <TabPanel></TabPanel>
+          <TabPanel></TabPanel>
+          <TabPanel></TabPanel>
+          <TabPanel></TabPanel>
+        </Tabs>
       </div>
     </div>
   );
